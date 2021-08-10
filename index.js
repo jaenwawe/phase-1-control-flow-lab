cb63943c559bfca19a5f5c8b233875ee74fc3b77
jaenwawe <nwawe.jae@gmail.com>
2021-08-10 17:20:44 -0500

    commit tree 1b9589a951700a0b67147fa4755d52f1fbb31263

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+let distance= 3000;
+let feet,city, gratuity;
+
+console.log("hello");
+
+
+function scuberGreetingForFeet(feet){
+
+
+if (feet <= 400){
+
+
+      return  'This one is on me!';
+}
+else if (feet <= 2500 && feet > 2000){
+     
+      return 'I will gladly take your thirty bucks.';
+}
+else if (feet > 2500)
+  return  'No can do.';
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+if (city == 'NYC')
+  return 'Ok, sounds good.'
+else 
+  return "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  if (tip == 'thanks for everything')
+    return "Bye.";
+  else if (tip =="not as generous")
+    return "Thank you.";
+  else if (tip == 'generous')
+    return "Thank you so much.";
 }
